a059cabcd17293ef60394abdd79bd50a0b9c3ed0
unknown <iryna.poplavska@estafet.com>
2020-04-09 17:25:48 +0300

    add check if desc fiels is null

--- src/main/java/com/estafet/openshift/boost/console/api/feature/model/Feature.java
+++ src/main/java/com/estafet/openshift/boost/console/api/feature/model/Feature.java
@@ -184,11 +184,8 @@ public class Feature {
 		public Feature build() {
 			Feature feature = new Feature();
 			feature.setFeatureId(featureId);
-			if (description.length() > 255) {
-				feature.setDescription(description.substring(0, 255));	
-			} else {
-				feature.setDescription(description);	
-			}
+			feature.setDescription( description==null ? null :description.length() > 255 ?
+					description.substring(0, 255):description);
 			feature.setStatus(status);
 			feature.setTitle(title);
 			feature.setUrl(url);
