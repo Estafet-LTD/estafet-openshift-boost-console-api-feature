354007bf90b69ac0570d82b8fde6b91429359a03
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-04-12 18:30:44 +0100

    log out the parameters for the query

--- src/main/java/com/estafet/openshift/boost/console/api/feature/dao/CommitDAO.java
+++ src/main/java/com/estafet/openshift/boost/console/api/feature/dao/CommitDAO.java
@@ -6,6 +6,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import com.estafet.openshift.boost.console.api.feature.model.RepoCommit;
@@ -13,6 +15,8 @@ import com.estafet.openshift.boost.console.api.feature.model.RepoCommit;
 @Repository
 public class CommitDAO {
 
+	private static final Logger log = LoggerFactory.getLogger(CommitDAO.class);
+	
 	@PersistenceContext
 	private EntityManager entityManager;
 
@@ -31,6 +35,8 @@ public class CommitDAO {
 	}
 	
 	public RepoCommit getCommit(String repo, String sha) {
+		log.info("repo - " + repo);
+		log.info("sha - " + sha);
 		TypedQuery<RepoCommit> query = entityManager
 				.createQuery("select c from RepoCommit c where c.repo.name = :repo and c.sha = :sha", RepoCommit.class);
 		List<RepoCommit> commits = query.setParameter("repo", repo).setParameter("sha", sha).getResultList();
